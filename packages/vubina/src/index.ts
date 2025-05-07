a705522f3b5d4bddda7c0e90e8190caa5338b8e9
Navid Talebian <navidt81@gmail.com>
2025-05-07 18:56:40 +0330

    feat(vubina): impliment vite plugin in index file

--- packages/vubina/src/index.ts
+++ packages/vubina/src/index.ts
@@ -1,1 +1,14 @@
-export { analyzeProject } from '@vubina/insights'
+import { getInsights } from '@vubina/insights';
+import { Plugin } from 'vite';
+
+export default (rootDir = process.cwd()): Plugin => {
+    return {
+        name: 'vubina',
+        apply: 'serve',
+
+        buildStart: async () => {
+            const result = await getInsights(rootDir);
+            console.log(result);
+        }
+    };
+};
