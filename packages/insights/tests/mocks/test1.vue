9887d26b1a55ec5585625daad5fcc66903724747
Navid Talebian <navidt81@gmail.com>
2025-05-14 16:18:26 +0330

    change dir name mock with mocks

---
+++ packages/insights/tests/mocks/test1.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { computed, ref, watch } from 'vue';
+
+const ref1 = ref(0);
+const ref2 = ref('1');
+
+const c1 = computed(() => ref1.value + 1);
+
+const { pause, resume, stop } = watch(ref2, () => {
+    ref2.value = 'navid';
+});
+
+watch(ref1, () => {
+    ref1.value = 4;
+});
+
+watch(ref1, (value) => {
+    ref1.value = value;
+}, {
+    once: true,
+    immediate: true,
+});
+
+computed(() => 2 + 2);
+</script>
+
+<template>
+    <div class="">
+        Test {{ c1 }}
+    </div>
+</template>
