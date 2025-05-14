cee173d379927908a57dedb1cdc715fa13d09e9a
Navid Talebian <navidt81@gmail.com>
2025-05-14 16:37:59 +0330

    put content of vue file into mock

--- packages/insights/tests/parser.test.ts
+++ packages/insights/tests/parser.test.ts
@@ -1,35 +1,10 @@
 import { describe, expect, it } from 'vitest';
 import { parseVueFile } from '../src/parser';
+import { readFileContent } from '../src/scanner';
 
 describe('parser', () => {
     it('should parse SFC and return block info', async () => {
-        const content = `
-            <script setup lang="ts">
-            import { computed, ref } from 'vue';
-
-            const props = defineProps<{
-                prop1: string;
-                prop2: number;
-            }>();
-
-            const ref1 = ref('test');
-            const ref2 = ref<number>(0);
-            const ref3 = ref(props.prop1);
-
-            const computed1 = computed(() => ref2.value + 1);
-            </script>
-
-            <template>
-                <div class="">
-                    Test 1
-                </div>
-                <div class="">
-                    {{ ref1 }}
-                    {{ ref3 }}
-                    {{ computed1 }}
-                </div>
-            </template>
-        `;
+        const content = await readFileContent(`${import.meta.dirname}/mocks/test1.vue`);
 
         const result = await parseVueFile('test1', content);
 
