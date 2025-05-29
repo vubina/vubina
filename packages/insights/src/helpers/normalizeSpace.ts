adee5262f4a291cc50af92f3da3525b9e6891e93
Navid Talebian <navidt81@gmail.com>
2025-05-29 18:21:04 +0330

    add sliceContent fn and reomve helper functions

--- packages/insights/src/helpers/normalizeSpace.ts
+++
@@ -1,8 +0,0 @@
-import type { Span } from 'oxc-parser';
-
-export function normalizeSpace<T extends Span>(content: T, offset: number): Span {
-    return {
-        start: content.start - offset,
-        end: content.end - offset,
-    };
-}
