3bb0a768c87792626493412a9b8552e6961695ed
Navid Talebian <navidt81@gmail.com>
2025-05-29 18:18:32 +0330

    test: rewrite watch tests

--- packages/insights/tests/parser/watch.test.ts
+++ packages/insights/tests/parser/watch.test.ts
@@ -1,34 +1,88 @@
+import { parseSync } from 'oxc-parser';
+import { extractWatchers } from 'packages/insights/src/analyzers/watch';
 import { parseVueFile } from 'packages/insights/src/parser';
 import { describe, expect, it } from 'vitest';
+import { sliceContent } from '../utils';
 
 describe('analyzeWatch - Vue <script setup> watch parser', () => {
-    const watchers = [
-        `watch(ref1,() => ref1.value++)`,
-        `watch(ref1,() => {
-            ref1.value = ref2.value + 1
-        })`,
-        `watch(ref1, (value) => {
-            ref1.value = value;
-        }, {
+    const ref1 = 'ref1';
+    const ref2 = 'ref2';
+
+    const callback1 = '() => ref1.value++';
+    const callback2 = `() => {ref1.value = ref2.value + 1}`;
+    const callback3 = `(value) => {ref1.value = value;}`;
+    const callback4 = `() => {ref2.value = 'navid';}`;
+
+    const config1 = `{
             once: true,
             immediate: true,
-        })`,
-        `const { pause, resume, stop } = watch(ref2, () => {
-            ref2.value = 'navid';
-        })`,
+        }`;
+
+    const watchers = [
+        `watch(${ref1},${callback1})`,
+        `watch(${ref1},${callback2})`,
+        `watch(${ref1}, ${callback3}, ${config1})`,
+        `const { pause, resume, stop } = watch(${ref2},${callback4})`,
+    ];
+
+    const expected = [
+        {
+            source: ref1,
+            callback: callback1,
+        },
+        {
+            source: ref1,
+            callback: callback2,
+        },
+        {
+            source: ref1,
+            callback: callback3,
+            config: config1,
+        },
+        {
+            source: ref2,
+            callback: callback4,
+        },
     ];
 
     it('should extract full watch lines from script block', async () => {
         const content = `<script setup lang="ts">\n${watchers.join('\n')}\n</script>`;
         const { script } = await parseVueFile('test.vue', content);
 
-        expect(script?.watchers.length).toBe(4);
+        expect(script?.watchers.length).toBe(watchers.length);
 
         if (script) {
             const { watchers: parsedWatchers } = script;
 
             for (const [i, watcher] of watchers.entries()) {
-                expect(watcher).toContain(parsedWatchers[i].content);
+                const { start, end } = parsedWatchers[i].content;
+                const watchContent = script.content.slice(start, end);
+                expect(watcher).toContain(watchContent);
+            }
+        }
+    });
+
+    it('should extract type,content, source, callback and config accurately', () => {
+        const parsedWatchers = watchers.map(watcher => extractWatchers(parseSync('test.js', watcher).program.body)[0]);
+
+        expect(parsedWatchers.length).toBe(expected.length);
+
+        for (const [i, parsedWatcher] of parsedWatchers.entries()) {
+            const content = watchers[i];
+            const { source, callback, config } = expected[i];
+
+            const parsedSource = sliceContent(content, parsedWatcher.source);
+            expect(parsedSource).toBe(source);
+
+            const parsedCallback = sliceContent(content, parsedWatcher.callback);
+            expect(parsedCallback).toEqual(callback);
+
+            if (config) {
+                expect(parsedWatcher.config).not.toBe(undefined);
+                if (parsedWatcher.config) {
+                    const parsedConfig = sliceContent(content, parsedWatcher.config);
+                    expect(parsedConfig).toEqual(config);
+                }
             }
         }
     });
